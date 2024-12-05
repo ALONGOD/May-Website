db7c2ac44e1fc37ee0ebb4095af2baa9ac846a86
Alon Goddarde <alongoddard1@gmail.com>
2024-12-05 13:45:57 +0200

    Back to simple design

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//     const images = document.querySelectorAll('.gallery img');
+document.addEventListener('DOMContentLoaded', () => {
+    const images = document.querySelectorAll('.gallery img');
 
-//     images.forEach(image => {
-//         image.addEventListener('mouseover', () => {
-//             image.style.filter = 'brightness(1.2)';
-//         });
+    images.forEach(image => {
+        image.addEventListener('mouseover', () => {
+            image.style.filter = 'brightness(1.2)';
+        });
 
-//         image.addEventListener('mouseout', () => {
-//             image.style.filter = 'brightness(1)';
-//         });
-//     });
-// });
+        image.addEventListener('mouseout', () => {
+            image.style.filter = 'brightness(1)';
+        });
+    });
+});
